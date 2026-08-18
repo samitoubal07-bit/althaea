2c0b36eb1cb8a80c0e83d1a3dbe719c99842319c
Sami <toubalsami9@gmail.com>
2026-08-18 19:11:22 +0100

    initial project setup

---
+++ src/test/java/com/Althaea/Althaea/AlthaeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Althaea.Althaea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlthaeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
